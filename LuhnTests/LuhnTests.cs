6bec00c4ebb6cef7b2a96ad71b2a225c64fdfbcc
Daniel Östlund <rag3rac3r@gmail.com>
2014-05-29 04:49:04 +0200

    Cleaned up the code for the tests.

--- LuhnTests/LuhnTests.cs
+++ LuhnTests/LuhnTests.cs
@@ -16,164 +16,121 @@ namespace LuhnTests
     public class LuhnTests
     {
         // Empty string
-        [Test(Description = "Test empty string to Something")]
+
+        [Test(Description = "Test Reverse")]
+        [Category("Utils")]
+        public void TestReverser()
+        {
+            StringAssert.IsMatch(Luhn.Luhn.Reverse("abcdef"), "fedcba", "Reverse string");
+        }
+
+        [Test(Description = "Test Cleaner")]
+        [Category("Utils")]
+        public void TestCleaner()
+        {
+            StringAssert.IsMatch(Luhn.Luhn.Clean("a1b2c3d4"), "1234", "Clean string");
+        }
+
+        #region LuhnVal
+
+        [Test(Description = "Test if LuhnVal throws exception on empty parameter")]
+        [Category("Validator")]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void TestLuhnValEmpty()
+        public void LuhnValEmptyParameter()
         {
             Luhn.Luhn.LuhnVal("");
         }
 
+        [Test(Description="Valid VISA card")]
+        [Category("Validator")]
+        public void IsValidVISA()
+        {
+            Assert.IsTrue(Luhn.Luhn.LuhnVal("4111 1111 1111 1111"), "Valid VISA card number");
+        }
+
+        [Test(Description = "Invalid VISA card number")]
+        [Category("Validator")]
+        public void IsInvalidVISA()
+        {
+            Assert.IsFalse(Luhn.Luhn.LuhnVal("4111 1111 1111 1112"), "Invalid VISA card number");
+        }
+
+        [Test(Description = "Test validation of Org Numbers (valid)")]
+        [Category("Validator")]
+        [TestCase("202100-5521", "Valid org number for Försäkringskassan")]
+        [TestCase("556196-1599", "Valid org number for SJ")]
+        [TestCase("556458-0040", "Valid org number for TeliaSonera Network Sales")]
+        public void IsValidOrgNumber(string number, string msg)
+        {
+            Assert.IsTrue(Luhn.Luhn.LuhnVal(number), msg);
+        }
+
+        [Test(Description = "Test validation of Org numbers (invalid)")]
+        [Category("Validator")]
+        [TestCase("202100-5522", "Invalid org number for Försäkringskassan")]
+        [TestCase("556196-1598", "Invalid org number for SJ")]
+        [TestCase("556458-0041", "Invalid org number for TeliaSonera Network Sales")]
+        public void IsInvalidOrgNumber(string number, string msg)
+        {
+            Assert.IsFalse(Luhn.Luhn.LuhnVal(number), msg);
+        }
+
+        [Test(Description = "Test validatoin of Bankgiro numbers (valid)")]
+        [Category("Validator")]
+        [TestCase("181-0993", "Valid bankgiro number for Försäkringskassa")]
+        [TestCase("785-8426", "Valid bankgiro number for SJ")]
+        public void IsValidBGNumber(string number, string msg)
+        {
+            Assert.IsTrue(Luhn.Luhn.LuhnVal(number), msg);
+        }
+
+        [Test(Description = "Test validation of bankgiro numbers (invalid)")]
+        [Category("Validator")]
+        [TestCase("181-0994", "Invalid bankgiro number for Försäkringskassan")]
+        [TestCase("785-8427", "Invalid bankgiro number for SJ")]
+        public void IsInvalidBGNumber(string number, string msg)
+        {
+            Assert.IsFalse(Luhn.Luhn.LuhnVal(number), msg);
+        }
+
+        #endregion
+
+        #region LuhnGen
+        
         [Test(Description = "Test empty string to LuhnGen")]
+        [Category("Generator")]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void TestLuhngenEmpty()
         {
             Luhn.Luhn.LuhnGen("");
         }
 
-        [Test(Description = "Test Reverse")]
-        public void TestReverse()
-        {
-            StringAssert.IsMatch(Luhn.Luhn.Reverse("abcdef"), "fedcba", "Reverse string");
-        }
-
-        [Test(Description = "Test Cleaner")]
-        public void TestCleaner()
-        {
-            StringAssert.IsMatch(Luhn.Luhn.Clean("a1b2c3d4"), "1234", "Clean string");
-        }
-
-        #region LuhnVal, Valid
-
-        [Test(Description="Valid VISA card")]
-        [Category("Valid")]
-        public void TestIsValidVISA()
-        {
-            Assert.IsTrue(Luhn.Luhn.LuhnVal("4111 1111 1111 1111"), "Valid VISA card number");
-        }
-
-        [Test(Description="Valid org number Försäkringskassan")]
-        [Category("Valid")]
-        public void TestIsValidFKOrg()
-        {
-            Assert.IsTrue(Luhn.Luhn.LuhnVal("202100-5521"), "Valid org number for Försäkringskassan");
-        }
-
-        [Test(Description="Valid bankgiro number Försäkringskassan")]
-        [Category("Valid")]
-        public void TestIsValidFKBG()
-        {
-            Assert.IsTrue(Luhn.Luhn.LuhnVal("181-0993"), "Valid bankgiro number for Försäkringskassa");
-        }
-
-        [Test(Description="Valid org number SJ")]
-        [Category("Valid")]
-        public void TestIsValidSJOrg()
-        {
-            Assert.IsTrue(Luhn.Luhn.LuhnVal("556196-1599"), "Valid org number for SJ");
-        }
-
-        [Test(Description = "Valid bankgiro numberSJ")]
-        [Category("Valid")]
-        public void TestIsValidSJBG()
-        {
-            Assert.IsTrue(Luhn.Luhn.LuhnVal("785-8426"), "Valid bankgiro number for SJ");
-        }
-
-        [Test(Description = "Valid org number TeliaSonera Network Sales")]
-        [Category("Valid")]
-        public void TestIsValidTeliaSonera()
-        {
-            Assert.IsTrue(Luhn.Luhn.LuhnVal("556458-0040"), "Valid org number for TeliaSonera Network Sales");
-        }
-
-        #endregion
-
-        #region LuhnVal, Invalid
-        [Test(Description="Invalid VISA card number")]
-        [Category("Invalid")]
-        public void TestIsInvalidVISA()
-        {
-            Assert.IsFalse(Luhn.Luhn.LuhnVal("4111 1111 1111 1112"), "Invalid VISA card number");
-        }
-
-        [Test(Description="Invalid org number Försäkringskassan")]
-        [Category("Invalid")]
-        public void TestIsInvalidFKOrg()
-        {
-            Assert.IsFalse(Luhn.Luhn.LuhnVal("202100-5522"), "Invalid org number for Försäkringskassan");
-        }
-
-        [Test(Description="Invalid bankgiro number Försäkringskassan")]
-        [Category("Invalid")]
-        public void TestIsInvalidFKBG()
-        {
-            Assert.IsFalse(Luhn.Luhn.LuhnVal("181-0994"), "Invalid bankgiro number for Försäkringskassa");
-        }
-
-        [Test(Description="Invalid org number SJ")]
-        [Category("Invalid")]
-        public void TestIsInvalidSJOrg()
-        {
-            Assert.IsFalse(Luhn.Luhn.LuhnVal("556196-1598"), "Invalid org number for SJ");
-        }
-
-        [Test(Description="Invalid bankgiro number SJ")]
-        [Category("Invalid")]
-        public void TestIsInvalidSJBG()
-        {
-            Assert.IsFalse(Luhn.Luhn.LuhnVal("785-8427"), "Invalid bankgiro number for SJ");
-        }
-
-        [Test(Description = "Invalid org number for TeliaSonera Network Sales")]
-        [Category("Invalid")]
-        public void TestIsInvalidTeliaSonera()
-        {
-            Assert.IsFalse(Luhn.Luhn.LuhnVal("556458-0041"), "Invalid org number for TeliaSonera Network Sales");
-        }
-        #endregion
-
-        #region LuhnGen
-        [Test(Description="Generate VISA checksum")]
-        [Category("Generate Checksum")]
+        [Test(Description="Generate VISA card checksum")]
+        [Category("Generator")]
         public void TestGenVISAChecksum()
         {
             StringAssert.IsMatch(Luhn.Luhn.LuhnGen("4111 1111 1111 111"), "1", "VISA card number");
         }
-        
-        [Test(Description="Generate FK Org number checksum")]
-        [Category("Generate Checksum")]
-        public void TestGenFKOrgChecksum()
+
+        [Test(Description = "Generate Org number checksum")]
+        [Category("Generator")]
+        [TestCase("202100-552", "1", "Org number for Försäkringskassan")]
+        [TestCase("556196-159", "9", "Org number for SJ")]
+        [TestCase("556458-004", "0", "Org number for TeliaSonera Network Sales")]
+        public void GenerateOrgNumberChecksum(string number, string checksum, string msg)
         {
-            StringAssert.IsMatch(Luhn.Luhn.LuhnGen("202100-552"), "1", "Org number for Försäkringskassa");
+            StringAssert.IsMatch(Luhn.Luhn.LuhnGen(number), checksum, msg);
         }
 
-        [Test(Description="Generate FK bankgiro number checksum")]
-        [Category("Generate Checksum")]
-        public void TestGenFKBGChecksum()
+        [Test(Description = "Generate BG number checksum")]
+        [Category("Generator")]
+        [TestCase("181-099", "3", "Bankgiro number for Försäkringskassan")]
+        [TestCase("785-842", "6", "Bankgiro number for SJ")]
+        public void GenerateBGNumberChecksum(string number, string checksum, string msg)
         {
-            StringAssert.IsMatch(Luhn.Luhn.LuhnGen("181-099"), "3", "Bankgiro number for Försäkringskassan");
+            StringAssert.IsMatch(Luhn.Luhn.LuhnGen(number), checksum, msg);
         }
 
-        [Test(Description="Generate SJ org number checksum")]
-        [Category("Generate Checksum")]
-        public void TestGenSJOrgChecksum()
-        {
-            StringAssert.IsMatch(Luhn.Luhn.LuhnGen("556196-159"), "9", "Org number for SJ");
-        }
-
-        [Test(Description="Generate SJ bankgiro number checksum")]
-        [Category("Generate Checksum")]
-        public void TestGenSJBGChecksum()
-        {
-            StringAssert.IsMatch(Luhn.Luhn.LuhnGen("785-842"), "6", "Bankgiro number for SJ");
-        }
-
-        [Test(Description = "Generate TeliaSonera Network Sales checksum")]
-        [Category("Generate Checksum")]
-        public void TestGenTeliaSoneraChecksum()
-        {
-            StringAssert.IsMatch(Luhn.Luhn.LuhnGen("556458-004"), "0", "Org number for TeliaSonera Network Sales");
-        }
         #endregion
     }
 }
